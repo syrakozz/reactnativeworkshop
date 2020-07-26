dbab52cc65cdd641b5f6a3d72cd22db0aca0a3bf
expert <expert-way@hotmail.com>
2020-07-26 11:16:18 +0300

    view code

---
+++ App.js
@@ -0,0 +1,45 @@
+import React,{Component} from 'react';
+import {View,SafeAreaView} from 'react-native'
+class HelloWorldApp extends Component {
+
+  render(){
+    return (
+    <SafeAreaView style={{
+        backgroundColor:"pink",
+        flex:1,
+        flexDirection:"row",
+      //  justifyContent:"center",
+       // alignItems:"stretch"
+    }}>
+
+        <View style={{
+            backgroundColor:"red",
+            height:150,
+            width:50
+        }}>
+
+        </View>
+
+        <View style={{
+            backgroundColor:"green",
+            height:50,
+            width:50
+        }}>
+
+        </View>
+
+        <View style={{
+            backgroundColor:"blue",
+           height:50,
+            width:50
+            //width:50
+        }}>
+
+        </View>
+
+    </SafeAreaView>
+    )
+  }
+
+}
+export default HelloWorldApp;
